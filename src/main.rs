c4d79217ffb39d4c9fe458a565985629c4dd463d
Aleksandr Skoropletov <alexander.skoropletov@1win.pro>
2023-05-25 21:50:47 +0300

    - users in common store - colored logging

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use dotenv::dotenv;
 use std::env;
 use warp::{serve, Filter};
 
-// each module represents an entire API route
 mod common {
     pub mod errors;
     pub mod responses;
     pub mod stdout;
 }
 mod data_store;
+// each module represents an entire API route. TODO: move to router.rs
 mod auth;
 mod currency;
 mod health;
@@ -30,7 +30,10 @@ async fn main() {
 
     let port: u16 = match env::var("PORT") {
         Ok(val) => val.parse::<u16>().unwrap_or(3030),
-        Err(_) => 3030,
+        Err(_) => {
+            common::stdout::warn("PORT not set, defaulting to 3030", ());
+            3030
+        },
     };
 
     serve(
--- src/user/model.rs
+++ src/user/model.rs
@@ -2,7 +2,7 @@ use crate::{data_store::Store, common::stdout};
 
 pub async fn get_list(uid: String, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
     let result = store.users.read();
-    stdout::info("User uid", &uid);
+    stdout::info("User uid", uid);
     stdout::info("Users", &result);
     Ok(warp::reply::json(&*result))
 }
